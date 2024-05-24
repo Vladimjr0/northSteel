34b221f8faac1cc6064c0d6102e678599daf324b
Vladimjr0 <159789144+Vladimjr0@users.noreply.github.com>
2024-05-24 12:27:05 +0300

    Add files via upload

---
+++ notes/src/main/java/com/shevcov/notes/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.shevcov.notes.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .servers(List.of(new Server().url("http://locahost:8080")
+                        )
+                )
+                .info(
+                        new Info().title("Заметки")
+                );
+    }
+}
+
